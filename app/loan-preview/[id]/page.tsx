db08f02f82027f1b34acc2c0cee7b71628c5e0c7
CodeMat17 <codemat.biz@gmail.com>
2025-06-24 15:26:00 +0100

    updated

--- app/loan-preview/[id]/page.tsx
+++ app/loan-preview/[id]/page.tsx
@@ -2,25 +2,16 @@ import PreviewCoreLoan from "@/components/coreLoans/PreviewCoreLoan";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { Id } from "@/convex/_generated/dataModel";
+import { notFound } from "next/navigation";
 
-export default async function PreviewLoan({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function Page({ params }: { params: { id: string } }) {
   const loanId = params.id as Id<"coreLoans">;
 
   const loan = await fetchQuery(api.coreLoans.getCoreLoanById, {
     loanId,
   });
 
-  if (!loan) {
-    return (
-      <div className='italic py-20 px-4 text-center animate-pulse'>
-        Loan not found.
-      </div>
-    );
-  }
+  if (!loan) return notFound();
 
   return (
     <div className='w-full max-w-6xl mx-auto px-4 py-8'>
